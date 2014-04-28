8744f6a1eed2ca5d30f07a7a96ce5ec78df39ef0
Pelly <marcelo.eduardo.gonzalez@gmail.com>
2014-04-28 09:52:40 -0300

    agregamos permisos al ability, solucionamos algunos problemas de  vista cursos e inscriptos

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,12 +1,20 @@
 class Ability
+ 
   include CanCan::Ability
-
+ 
   def initialize(user)
-    user.roles.each { |role|
-      role.permissions.each { |permission|
-        can permission.conduct.to_sym, permission.regulator.constantize
-      }
-    }
-
+    user ||= User.new # guest user
+    if user.role? :admin
+      can :manage, :all
+      #can :update, User do |user2|
+      #  user2.try(:id) == user.id
+      #end    
+    elsif user.role? :dpd
+      can :manage, [Curso]
+      can :manage, [Persona]
+      can :manage, [InstitucionOferente]
+      can :manage, [Escuela]
+      can :manage, [Titulo]
+    end
   end
 end
