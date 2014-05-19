f30d35a7c659479c76b3348d1ce79d5f0f9e8353
Pelly <marcelo.eduardo.gonzalez@gmail.com>
2014-05-19 10:09:58 -0300

    castellanizacion

--- app/models/curso.rb
+++ app/models/curso.rb
@@ -1,4 +1,5 @@
 class Curso < ActiveRecord::Base
+	
   attr_accessible :capacitadores, :carga_horaria_no_presencial, :carga_horaria_presencial, :destinatarios, :email, :fechas_y_horarios, :institucion_oferente, :localidad_id, :nro_expediente, :resenia, :tel_contacto, :disponible, :cupo_real, :cupo_inscripcion, :nombre
   attr_accessible :institucion_oferente_id, :lugar_de_realizacion 
   attr_accessible :finalizado
@@ -11,5 +12,6 @@ class Curso < ActiveRecord::Base
   has_many :persona, :through => :persona_curso
 
   validates :cupo_inscripcion, :presence => true
+  validates :nro_expediente, :presence => true, uniqueness: true
 
 end
